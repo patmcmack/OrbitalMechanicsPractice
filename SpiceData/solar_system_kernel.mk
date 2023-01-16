aff437a7c1b85cdff910e7bc85d231b4694ded68
patmcmack <patmcmack@users.noreply.github.com>
2023-01-15 18:31:32 -0600

    Spice files for testing

---
+++ SpiceData/solar_system_kernel.mk
@@ -0,0 +1,9 @@
+\begindata
+
+KERNELS_TO_LOAD=(
+
+'/home/mcmackinp/Documents/orbitalMechanics/SpiceData/latest_leapseconds.tls',
+'/home/mcmackinp/Documents/orbitalMechanics/SpiceData/de432s.bsp'
+)
+
+\begintext
